d17718d3389b784b7508a0100b522f37af99c6f4
Dan Andrei <andreidani96@gmail.com>
2018-03-26 17:59:37 +0300

    Basic project structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { logger } from 'redux-logger'
+import reducer from './reducers'
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const middleware = [
+];
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware),
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
